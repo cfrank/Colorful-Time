86ca10e9dc6b50ea8d4aabf8dac0c9bac402ec9d
Chris Frank <c@cs-MacBook-Pro.local>
2015-06-13 12:11:06 +0300

    "DRY" time.js
    Conform time.js to the DRY approach

--- time.js
+++ time.js
@@ -3,24 +3,21 @@ function getTime()
 	var body 		= document.body,
 		container	= document.getElementById("content"),
 		date		= new Date(),
-		hours		= date.getHours(),
-		minutes		= date.getMinutes(),
-		seconds		= date.getSeconds(),
+		nums		= [date.getHours(), date.getMinutes(), date.getSeconds()];
 		timeCont	= document.getElementById("time"),
 		hexCont		= document.getElementById("color");
 
 	// For formatting purposes
-	if(hours < 10)
-		hours = "0" + hours;
-	if(minutes < 10)
-		minutes = "0" + minutes;
-	if(seconds < 10)
-		seconds = "0" + seconds;
+	for(var i = 0, len = nums.length; i < len; ++i)
+	{
+		if(nums[i] < 10)
+			nums[i] = "0" + nums[i];
+	}
 
-	var hex = "#" + "" + hours + minutes + seconds;
-	
+	var hex = "#" + "" + nums[0] + nums[1] + nums[2];
+
 	body.style.background = hex;
-	timeCont.innerHTML = hours + " : " + minutes + " : " + seconds;
+	timeCont.innerHTML = nums[0] + " : " + nums[1] + " : " + nums[2];
 	hexCont.innerHTML = hex;
 
 	setTimeout(function(){getTime();} , 1000);
